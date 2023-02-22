f5e1d63f07859d88bafe9bcd85f725b9b12caadd
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-02-22 18:46:07 -0300

    Initial Commit

---
+++ testswiject/testswijectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testswijectApp.swift
+//  testswiject
+//
+//  Created by Jonn Alves on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct testswijectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
